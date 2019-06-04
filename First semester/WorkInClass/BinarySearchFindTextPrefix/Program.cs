be3392487a405e3f9e037a52a7867f8ecebb3c45
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-04 11:35:51 +0300

    Added work in class

---
+++ First semester/WorkInClass/BinarySearchFindTextPrefix/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace BinarySearchFindTextPrefix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "E:\\ТУ - програмиране\\WorkInClass\\BinarySearchFindTextPrefix\\bin\\Debug\\file.txt";
+            string[] names = File.ReadAllLines(path);
+            string prefix = "Ivan";
+
+            int counter = 0;
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                bool equal = true;
+                for (int k = 0; k < prefix.Length; k++)
+                {
+                    if (names[i][k] == prefix[k])
+                    {
+                        counter++;
+                        equal = false;
+                        break;
+                    }
+                    if (equal)
+                    {
+                        Console.WriteLine(names[i]);
+                    }
+                }
+            }
+        }
+    }
+}
