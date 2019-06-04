debd5b514c7a3c95dc8b1dc0f7f315add64fcba7
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-04 11:34:44 +0300

    Added work in class

---
+++ First semester/WorkInClass/ArrayFromPeople/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    public struct Person
+    {
+        public string name;
+        public int identifyNumber;
+    }
+    static void Main(string[] args)
+    {
+        Person[] people = new Person[3];
+        for (int i = 1; i <= 3; i++)
+
+        {
+            people[i].name = Console.ReadLine();
+            people[i].identifyNumber = int.Parse(Console.ReadLine());
+        }
+    }
+}
