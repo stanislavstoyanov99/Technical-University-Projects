550276e5bcf6bedc6308e92ca4d4589bd8c29ca4
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-04 11:33:52 +0300

    Added work in class

---
+++ First semester/WorkInClass/ArrayExample/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ArrayExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] array = new int[100];
+            //for (int count = 1; count <= 100; count++)
+            //{
+            //    array[count] = count;
+            //}
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    Console.WriteLine(array);
+            //}
+
+            //string input = Console.ReadLine();
+            //for (int letterIndex = 0; letterIndex < input.Length; letterIndex++)
+            //{
+            //    char letter = input[letterIndex];
+            //    Console.WriteLine(letter);
+            //}
+
+        }
+
+        static int FindIndex(int[] arr, int val)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == val)
+                {
+                    return i;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+        }
+    }
+}
