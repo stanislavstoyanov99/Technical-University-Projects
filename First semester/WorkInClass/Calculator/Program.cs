5576cebfeb22340cfb8839eb5dd6bd96ec420646
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-04 11:37:38 +0300

    Added work in class

---
+++ First semester/WorkInClass/Calculator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string expression = Console.ReadLine();
+        double totalSum = 0;
+        string fullDigit = string.Empty;
+
+        for (int i = 0; i < expression.Length; i++)
+        {
+            char symbol = expression[i];
+            bool isDigit = char.IsDigit(symbol);
+            fullDigit += symbol;
+            if (!isDigit)
+            {
+                totalSum += double.Parse(fullDigit);
+                if (symbol == '=')
+                {
+                    Console.WriteLine($"{totalSum}");
+                }
+                else if (symbol == '+')
+                {
+                    totalSum += double.Parse(fullDigit);
+                }
+                else if (symbol == '-')
+                {
+                    totalSum -= double.Parse(fullDigit);
+                }
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
