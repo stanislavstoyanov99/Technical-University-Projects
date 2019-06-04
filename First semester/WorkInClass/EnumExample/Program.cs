5c25f9e86714306d45280d9eb1e86c9e12d6de94
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-04 11:38:51 +0300

    Added work in class

---
+++ First semester/WorkInClass/EnumExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace enumExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DaysOfWeek lectureDay;
+            lectureDay = DaysOfWeek.Monday;
+
+            DaysOfWeek.IsDefined(typeof(DaysOfWeek), 8);
+        }
+        enum DaysOfWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
