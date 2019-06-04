c55960bd3b5f0ca2a1255d9f3345b3e8f9fff764
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-04 11:38:34 +0300

    Added work in class

---
+++ First semester/WorkInClass/ClassExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class RationalNumber
+{
+    private int numerator, denominator;
+    public RationalNumber()
+    {
+        numerator = 0;
+        denominator = 1;
+    }
+    public RationalNumber(int num, int denom)
+    {
+        numerator = num;
+        denominator = denom;
+    }
+    public decimal ToDecimal()
+    {
+        return (decimal)numerator / denominator;
+    }
+    public override string ToString()
+    {
+        return string.Format("{0}/{1}", numerator, denominator);
+    }
+
+    static void Main(string[] args)
+    {
+        RationalNumber a = new RationalNumber();
+        RationalNumber b = new RationalNumber(1, 4);
+        RationalNumber c = null;
+        Console.WriteLine("a={0}, decimal:{1}", a, a.ToDecimal());
+        Console.WriteLine("b={0}, decimal: {1}", b, b.ToDecimal());
+        if (c != null)
+        {
+            Console.WriteLine("c={0}, decimal:{1}", c, c.ToDecimal());
+        }
+    }
+}
