96985b8b1aeb086100340c8e7ad7d7eb370b0f27
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-04 11:37:21 +0300

    Added work in class

---
+++ First semester/WorkInClass/BubbleSort/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace bubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new decimal[] { 5, 8, 1, 6, 2 };
+            BubbleSort(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+        static void BubbleSort(decimal[]array)
+        {
+            bool isSorted = true;
+            do
+            {
+                isSorted = true;
+                for (int i = 1; i < array.Length; i++)
+                {
+                    if (array[i - 1] > array[i])
+                    {
+                        //Размяна на стойностите на две променливи
+                        isSorted = false;
+                        var temp = array[i];
+                        array[i] = array[i - 1];
+                        array[i - 1] = temp;
+                    }
+                }
+            }
+            while (!isSorted);
+        }
+    }
+}
