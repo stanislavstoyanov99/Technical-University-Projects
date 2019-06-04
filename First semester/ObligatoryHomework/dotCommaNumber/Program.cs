8597b56edbc7a9db9464c0a710a2a861aa694a2a
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-04 11:31:22 +0300

    Added homework stuff

---
+++ First semester/ObligatoryHomework/dotCommaNumber/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace dotCommaNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please, enter file location: "); //"E:\\ТУ - програмиране\\ObligatoryHomework\\dotCommaNumber\\bin\\Debug\\file.txt"
+
+            string fileLocation = Console.ReadLine();
+            StreamReader reader = new StreamReader(fileLocation);
+
+            using (reader)
+            {
+                int dotsCounter = 0;
+                int commasCounter = 0;
+                int numbersCounter = 0;
+
+                bool isDigit = false;
+                do
+                {
+                    char element = (char)reader.Read();
+
+                    if (element == '.') // check whether the element is dot
+                    {
+                        if (isDigit)
+                        {
+                            isDigit = false;
+                            numbersCounter++;
+                        }
+                        dotsCounter++;
+                    }
+                    else if (element == ',') // check whether the element is comma
+                    {
+                        if (isDigit)
+                        {
+                            isDigit = false;
+                            numbersCounter++;
+                        }
+                        commasCounter++;
+                    }
+                    else if (Char.IsDigit(element)) // check whether the element is digit
+                    {
+                        isDigit = true;
+                    }
+                }
+                while (!reader.EndOfStream); // until the end of stream
+
+                if (isDigit) //final if-statement if the last element of the file is a number
+                {
+                    numbersCounter++;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Statistics:");
+                Console.WriteLine($"Total number of dots is: {dotsCounter}");
+                Console.WriteLine($"Total number of commas is: {commasCounter}");
+                Console.WriteLine($"Total numbers: {numbersCounter}");
+            }
+        }
+    }
+}
---
+++ First semester/ObligatoryHomework/matrixSum/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace matrixSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("matrix.txt");
+            using (reader)
+            {
+                int rowCounter = 0;
+                int columnCounter = 0;
+
+                int evenSum = 0;
+                int oddSum = 0;
+
+                string line;
+                
+                while ((line = reader.ReadLine()) != null)
+                {
+                    rowCounter++;
+                    var element = line.Split(' ').Select(int.Parse);
+
+                    foreach (var number in element)
+                    {
+                        columnCounter++;
+                        if (rowCounter % 2 != 0 && number % 2 == 0)
+                        {
+                            evenSum += number;
+                        }
+                        if (columnCounter % 2 == 0 && number % 2 != 0)
+                        {
+                            oddSum += number;
+                        }
+                    }
+                    columnCounter = 0;
+                }
+
+                Console.WriteLine("The sum of even numbers on odd rows is: {0}", evenSum);
+                Console.WriteLine("The sum of odd numbers on even columns is: {0}", oddSum);
+            }
+        }
+    }
+}
+
