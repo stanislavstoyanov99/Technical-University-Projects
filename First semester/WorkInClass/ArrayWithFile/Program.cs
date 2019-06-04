dea0abfd5b4a3538bc79cf29511989c23e663018
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-04 11:35:14 +0300

    Added work in class

---
+++ First semester/WorkInClass/ArrayWithFile/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace arrayWithFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] array = new double[];
+        }
+    }
+}
