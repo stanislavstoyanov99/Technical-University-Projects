445d4613a6878f739feb7dd3c7b9317f7c2306d0
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-04 11:37:55 +0300

    Added work in class

---
+++ First semester/WorkInClass/ChangeSymbolA/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace ChangeSymbolA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string newFileContent = string.Empty;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == 'a')
+                {
+                    newFileContent += '_';
+                }
+                else
+                {
+                    newFileContent += text[i];
+                }
+            }
+            File.WriteAllText("C:\\Users\\User\\Desktop\\test.txt", newFileContent);
+        }
+    }
+}
