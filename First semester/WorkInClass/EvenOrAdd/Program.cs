8b25c9c371c1f6a400fad05f6e876dc51faafedb
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-04 11:39:29 +0300

    Added work in class

---
+++ First semester/WorkInClass/EvenOrAdd/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("The number is even!");
+        }
+        else
+        {
+            Console.WriteLine("The number is odd!");
+        }
+    }
+}
