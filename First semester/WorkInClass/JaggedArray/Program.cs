12d21997ed90d9d1855c42360e4d43a51d21c7ea
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-08 14:26:47 +0300

    Added work in class

---
+++ First semester/WorkInClass/JaggedArray/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[][] jaggedArray = new int[2][];
+        jaggedArray[0] = new int[3] { 1, 2, 3};
+        jaggedArray[1] = new int[1] { 4 };
+        foreach(var arr in jaggedArray)
+        {
+            foreach (var element in arr)
+            {
+                Console.Write($"{element} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
