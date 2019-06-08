dafd9fe6eaacf3f099fe62f497d6d1245d262908
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-08 14:23:56 +0300

    Added work in class

---
+++ First semester/WorkInClass/FileStreamExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace FileStreamExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream stream = new FileStream("test.txt", FileMode.Open);
+
+            for (int i = 0; i < stream.Length; i++)
+            {
+                char ch = (char)stream.ReadByte();
+                if (ch == 'a')
+                {
+                    stream.Position--;
+                    stream.WriteByte((byte)'_');
+                }
+            }
+
+            stream.Close();
+        }
+    }
+}
