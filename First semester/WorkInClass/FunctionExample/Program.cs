0d452f65777d4a4b838bc71bd563d7f1a7b811da
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-08 14:25:49 +0300

    Added work in class

---
+++ First semester/WorkInClass/FunctionExample/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace FunctionExample
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    int c = int.Parse(Console.ReadLine());
+        //    int d = int.Parse(Console.ReadLine());
+        //    int m = Min(c, d);
+        //    Console.WriteLine($"The smallest number is {m}");
+        //}
+        //static int Min(int a, int b)
+        //{
+        //    return a < b ? a : b;
+        //}
+
+        //static void Main(string[] args)
+        //{
+        //    int c = int.Parse(Console.ReadLine());
+        //    int d = int.Parse(Console.ReadLine());
+        //    int m = Max(c, d);
+        //    Console.WriteLine($"The biggest number is {m}");
+        //}
+        //static int Max(int a, int b)
+        //{
+        //    return a > b ? a : b;
+        //}
+
+        //static void Main(string[] args)
+        //{
+        //    int t = 5;
+        //    AddThenPrintValue(t, 1);
+        //    Console.WriteLine($"t = {t}");
+        //}
+
+        //static void AddThenPrintValue(int target, int value)
+        //{
+        //    target += value;
+        //    Console.WriteLine($"target = {target}");
+        //}
+
+        //static void Swap (ref int a, int b)
+        //{
+        //    int temp = a;
+        //    a = b;
+        //    b = temp;
+        //    Console.WriteLine($"a = {a}, b = {b}");
+        //}
+
+        //static void Main (string[] args)
+        //{
+        //    int x = 0, y = 5;
+        //    Swap(ref x, y);
+        //    Console.WriteLine($"x = {x}, y = {y}");
+        //}
+
+        //static void Main(string[] args)
+        //{
+        //    int v = int.Parse(Console.ReadLine()); //v = 5
+        //    int vv;
+        //    PrintSquared(v, out vv);
+        //    Console.WriteLine($"vv = {vv}"); // vv = 25
+        //}
+        //static void PrintSquared(int val, out int squared)
+        //{
+        //    squared = val * val; // squared = 25
+        //    Console.WriteLine($"squared = {squared}");
+        //}
+    }
+}
