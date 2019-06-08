323512f99ec6e825c6b211d80c85964ea9547be0
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-08 14:25:32 +0300

    Added work in class

---
+++ First semester/WorkInClass/FormatText/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace FormatText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileSource = Console.ReadLine();
+            var fileDestination = Console.ReadLine();
+
+            var contentSource = File.ReadAllText(fileSource);
+
+            string contentResult = string.Empty;
+            int d = 0;
+
+            for (int i = 0; i < contentSource.Length; i++)
+            {
+                contentResult += contentSource[i];
+                d++;
+
+                if (d == 80)
+                {
+                    contentResult += Environment.NewLine;
+                    d = 0;
+                }
+            }
+
+            File.WriteAllText(fileDestination, contentResult);
+        }
+    }
+}
