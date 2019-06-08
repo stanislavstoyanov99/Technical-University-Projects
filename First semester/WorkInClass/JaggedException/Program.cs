b37aa200202f46ebbaf8a844a1690b27be442ce0
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-08 14:27:03 +0300

    Added work in class

---
+++ First semester/WorkInClass/JaggedException/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace jaggedException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                try
+                {
+                    try
+                    {
+                        throw new Exception();
+                    }
+                    catch (MyException e)
+                    {
+                        // 1
+                    }
+                    catch (Exception e)
+                    {
+                        // 2
+                        throw new MyException(e.Message);
+                    }
+                }
+                catch (Exception e)
+                {
+                    //3
+                    throw;
+                }
+            }
+            catch (MyException e)
+            {
+                //4
+                throw;
+            }
+            catch (MyException e)
+            {
+                //5
+            }
+            catch (MyException e)
+            {
+                //6
+            }
+        }
+    }
+}
