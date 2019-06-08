e7ef63b63ab15b155fa0d52b35675f43ba5e2ce7
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-08 14:26:06 +0300

    Added work in class

---
+++ First semester/WorkInClass/Game/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int winingDoor = 0;
+        int emptyDoor = 0;
+        int secondChoiceDoor = 0;
+        int firstChoiceDoor = 0;
+
+        int wins = 0;
+        int secondWins = 0;
+
+        for (int count = 1; count <= 100; count++)
+        {
+            Random r = new Random((int)DateTime.Now.Ticks);
+            firstChoiceDoor = r.Next(1, 4);
+            if (winingDoor == firstChoiceDoor)
+            {
+                winingDoor = firstChoiceDoor;
+                wins++;
+            }
+            else
+            {
+                winingDoor = secondChoiceDoor;
+                wins++;
+            }
+
+            for (emptyDoor = 1; emptyDoor <= 3; emptyDoor++)
+            {
+                if (emptyDoor != firstChoiceDoor && emptyDoor != winingDoor)
+                {
+                    break;
+                }
+            }
+            for (secondChoiceDoor = 1; secondChoiceDoor <= 3; secondChoiceDoor++)
+            {
+                if (secondChoiceDoor != firstChoiceDoor && secondChoiceDoor != winingDoor)
+                {
+                    secondWins++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
