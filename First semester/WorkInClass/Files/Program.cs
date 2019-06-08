294e8ba119df5587b9fca05c5e4fc952169f8112
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-08 14:23:39 +0300

    Added work in class

---
+++ First semester/WorkInClass/Files/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System;
+using System.Security.Permissions;
+
+namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (File.Exists("storage.txt"))
+            {
+                var file = File.Open("storage.txt", FileMode.Truncate);
+                file.Close();
+            }
+            //if (File.Exists("storage.txt"))
+            //{
+            //    string stored = File.ReadAllText("storage.txt");
+            //    Console.WriteLine(stored);
+            //}
+            string s = Console.ReadLine();
+            File.WriteAllText("storage.txt", s);
+        }
+    }
+}
