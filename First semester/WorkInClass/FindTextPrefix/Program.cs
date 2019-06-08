3c94d1028a1fe76c0c123b92379bd7bd9d39ab5f
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-08 14:24:17 +0300

    Added work in class

---
+++ First semester/WorkInClass/FindTextPrefix/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace FindTextPrefix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "E:\\ТУ - програмиране\\WorkInClass\\FindTextPrefix\\bin\\Debug\\file.txt";
+            string [] names = File.ReadAllLines(path);
+
+            int counter = 0;
+            string prefix = "Ivan";
+            for (int i = 0; i < names.Length; i++)
+            {
+                bool equal = true;
+                for (int k = 0; k < prefix.Length; k++)
+                {
+                    if (names [i][k] == prefix[k])
+                    {
+                        counter++;
+                        equal = false;
+                        break;
+                    }
+                    if (equal)
+                    {
+                        Console.WriteLine(names[i]);
+                    }
+                }
+            }
+
+        }
+    }
+}
