6b8b767b2a0b8cffc2012d3acf41416f9919f5d4
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-08 14:25:16 +0300

    Added work in class

---
+++ First semester/WorkInClass/ForExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace forExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 20; i++)
+            {
+                if (i % 2 == 0 && i != 10)
+                {
+                    Console.WriteLine($"{i} ");
+                }
+            }
+        }
+    }
+}
