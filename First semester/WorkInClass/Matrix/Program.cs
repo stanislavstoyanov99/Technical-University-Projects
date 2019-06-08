a43ecdf2638cc5d8b994b309078f3629e162eb51
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-08 14:27:28 +0300

    Added work in class

---
+++ First semester/WorkInClass/Matrix/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] matrix = new int[10, 10];
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                matrix[i, j] = 10 * i + j;
+            }
+        }
+    }
+}
