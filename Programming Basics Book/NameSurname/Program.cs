6a876f6e0356c9358a4d1aca95e4f405cf5607f1
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-14 23:05:35 +0300

    Added Name Surname exercise

---
+++ Programming Basics Book/NameSurname/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NameSurname
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Name: Stanislav ");
+            Console.WriteLine();
+            Console.Write("Surname: Stoyanov ");
+            Console.WriteLine();
+        }
+    }
+}
