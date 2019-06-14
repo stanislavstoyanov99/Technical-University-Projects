497559b2775dfc4e957dd740b5a5cda7cf0c3438
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-14 23:04:01 +0300

    Added exercise Age After 10 Years

---
+++ Programming Basics Book/AgeAfter10Years/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AgeAfter10Years
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter age: ");
+            int age = int.Parse(Console.ReadLine());
+            DateTime newAge = new DateTime(age);
+
+            Console.WriteLine("The age after 10 years is: {0}", age + 10);
+        }
+    }
+}
