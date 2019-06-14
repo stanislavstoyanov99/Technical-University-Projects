2ed95a406f148bfb06d9a5854d4f5346c5f9dd4d
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-14 23:05:05 +0300

    Added Hello CSharp exercise

---
+++ Programming Basics Book/HelloCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добър ден!");
+        }
+    }
+}
