dfafdec962415f5eeb2db904a21749cd871675cf
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-14 23:04:28 +0300

    Added Date Time Example

---
+++ Programming Basics Book/DateTimeExample/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DateTimeExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentDate = DateTime.Now;
+            Console.WriteLine(currentDate.ToString(System.Globalization.CultureInfo.InvariantCulture));
+        }
+    }
+}
