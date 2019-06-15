eb5e428d150267e1ce200f8b2bcc4cdcf29a682a
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-15 14:30:31 +0300

    Added sqrt exercise

---
+++ Programming Basics Book/SqrtExample/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace sqrtExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = 12345;
+            Console.WriteLine(Math.Sqrt(number));
+        }
+    }
+}
