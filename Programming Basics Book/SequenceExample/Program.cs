7afb8fbf7d6c0249868048c590becf361708f824
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-15 14:29:52 +0300

    Added sequence example exercise

---
+++ Programming Basics Book/SequenceExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SequenceExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 2; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                else
+                {
+                    Console.WriteLine(i * -1);
+                }
+            }
+        }
+    }
+}
