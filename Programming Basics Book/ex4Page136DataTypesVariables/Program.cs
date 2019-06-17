d51ec10b1a43689b595b440e144c60c6d60bc225
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-17 11:40:36 +0300

    Added data types variables

---
+++ Programming Basics Book/ex4Page136DataTypesVariables/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ex4Page136DataTypesVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberInDec = 256;
+            int numberInHex = 0x100; // 256
+
+            Console.WriteLine(numberInDec); // 256
+            Console.WriteLine(numberInHex); // 256
+        }
+    }
+}
