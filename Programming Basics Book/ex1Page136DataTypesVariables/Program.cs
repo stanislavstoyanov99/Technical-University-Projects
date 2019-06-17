6d0da25c2445fff298627cbea2e5dae1df036e9e
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-17 11:39:55 +0300

    Added data types variables

---
+++ Programming Basics Book/ex1Page136DataTypesVariables/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace ex1Page136DataTypesVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Range of sbyte - -128 - 127
+            sbyte firstNumber = -115;
+            sbyte secondNumber = 112;
+            sbyte thirdNumber = 97;
+            sbyte fourthNumber = -44;
+
+            //Range of byte - 0 - 255
+            byte firstNumberAsByte = 112;
+            byte secondNumberAsByte = 97;
+            byte thirdNumberAsByte = 224;
+
+            //Range of short - -32_768 - 32_767
+            short firstNumberAsShort = -10_00;
+            short secondNumberAsShort = 20_000;
+            short thirdNumberAsShort = 1990;
+            short fourthNumberAsShort = -115;
+            short fifthNumberAsShort = 97;
+            short sixthNumberAsShort = 224;
+            short seventhNumberAsShort = 112;
+            short eightNumberAsShort = -44;
+
+            //Range of ushort - 0 - 65_535
+            ushort firstNumberAsUShort = 52130;
+            ushort secondNumberAsUShort = 97;
+            ushort thirdNumberAsUShort = 20_000;
+            ushort fourthNumberAsUShort = 224;
+            ushort fifthNumberAsUShort = 112;
+            ushort sixthNumberAsUShort = 1990;
+
+            //Range of int - -2147483648 - 2147483647
+            int firstNumberAsInt = 52130;
+            int secondNumberAsInt = -115;
+            int thirdNumberAsInt = 4825932;
+            int fourtNumberAsInt = 97;
+            int fifthNumberAsInt = -10_000;
+            int sixthNumberAsInt = 20_000;
+            int seventhNumberAsInt = 224;
+            int eightNumberAsInt = 970700000;
+            int ninethNumberAsInt = 112;
+            int tenthNumberAsInt = -44;
+            int eleventhNumberAsInt = -1000000;
+            int twelfthNumberAsInt = 1990;
+
+            //Range of uint 0 - 4294967295
+            int firstNumberAsUint = 52130;
+            int secondNumberAsUint = -115;
+            int thirdNumberAsUint = 4825932;
+            int fourtNumberAsUint = 97;
+            int fifthNumberAsUint = -10_000;
+            int sixthNumberAsUint = 20_000;
+            int seventhNumberAsUint = 224;
+            int eightNumberAsUint = 970700000;
+            int ninethNumberAsUint = 112;
+            int tenthNumberAsUint = -44;
+            int eleventhNumberAsUint = -1000000;
+            int twelfthNumberAsUint = 1990;
+
+            //Range of long -9223372036854775808 - 9223372036854775807
+            //Large numbers as you can see. :D
+
+            //Range of ulong - 0 - 18446744073709551615
+            //Even larger numbers. :D
+        }
+    }
+}
