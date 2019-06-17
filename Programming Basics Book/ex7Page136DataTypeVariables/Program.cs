aacc958a7e8e36269a45961402d1f0944724f92a
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-17 11:41:11 +0300

    Added data types variables

---
+++ Programming Basics Book/ex7Page136DataTypeVariables/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ex7Page136DataTypeVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Hello";
+            string secondGreeting = "World";
+            object obj = greeting + " " + secondGreeting;
+
+            Console.WriteLine(obj);
+        }
+    }
+}
