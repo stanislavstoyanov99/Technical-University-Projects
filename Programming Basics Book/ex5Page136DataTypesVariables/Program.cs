644ee2d0145354ff7df08598e2f21668375ec7c7
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-17 11:40:54 +0300

    Added data types variables

---
+++ Programming Basics Book/ex5Page136DataTypesVariables/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ex5Page136DataTypesVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char symbol = 'H'; // H in DEC is 72 and in Hex is 48
+            Console.WriteLine((int)symbol);
+        }
+    }
+}
