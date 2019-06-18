bb6f4313a614f58fc684151d36727a541302254a
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-19 00:10:05 +0300

    Added operands and expressions exercise

---
+++ Programming Basics Book/ex3page160OperandsExpressions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ex3page160OperandsExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int newNumber = number / 100;
+            int thirdDigit = newNumber % 10;
+
+            if (newNumber == 7)
+            {
+                Console.WriteLine("The third digit is 7.");
+            }
+            else
+            {
+                Console.WriteLine("The third digit is not 7.");
+            }
+        }
+    }
+}
