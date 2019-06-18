e8ac966588c442a238abbf5db243f163944065a0
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-19 00:10:22 +0300

    Added operands and expressions exercise

---
+++ Programming Basics Book/ex4page160OperandsExpressions/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ex4page160OperandsExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            bool isThirdDigit1Or0 = ((number >> 3) & 1) == 1;
+            if (isThirdDigit1Or0)
+            {
+                Console.WriteLine("The third bit of {0} is 1? - {1}", number, isThirdDigit1Or0);
+            }
+            else
+            {
+                Console.WriteLine("The third bit of {0} is not 1.", number);
+            }
+        }
+    }
+}
