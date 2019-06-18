ec2679278987ca8f4489de5544df901877fa589c
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-19 00:11:13 +0300

    Added data types and variables exercise

---
+++ Programming Basics Book/ex9Page137DataTypesVariables/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ex9Page137DataTypesVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstVariable = @"The ""use"" of quotation causes difficulties."; // exception for "", using quoted string
+            string secondVariable = "The \"use\" of quotation causes difficulties.";
+
+            Console.WriteLine(firstVariable);
+            Console.WriteLine(secondVariable);
+        }
+    }
+}
