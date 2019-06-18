8dab0aef98f5cfc2ab204289c0da529ed421e51e
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-19 00:09:49 +0300

    Added operands and expressions exercise

---
+++ Programming Basics Book/ex2page160OperandsExpressions/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ex2page160OperandsExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            if (number % 5 == 0 && number % 7 == 0)
+            {
+                Console.WriteLine("It is divided by 5 and 7.");
+            }
+            else
+            {
+                Console.WriteLine("It is not divided by 5 and 7.");
+            }
+        }
+    }
+}
