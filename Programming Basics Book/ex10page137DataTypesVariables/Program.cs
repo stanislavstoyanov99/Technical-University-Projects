d37162773382abf0065d71907f27b2e458167a32
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-19 00:11:36 +0300

    Added data types and variables exercise

---
+++ Programming Basics Book/ex10page137DataTypesVariables/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ex10page137DataTypesVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("    o o.o o   ");
+            Console.WriteLine("  o         o       ");
+            Console.WriteLine("  o         o       ");
+            Console.WriteLine("   o       o         ");
+            Console.WriteLine("     o    o       ");
+            Console.WriteLine("       o         ");
+        }
+    }
+}
