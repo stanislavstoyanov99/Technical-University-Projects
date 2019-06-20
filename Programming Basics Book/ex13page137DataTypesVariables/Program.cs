b0d4b3c41ea88a5b22c9ac9354101da3e2da8f56
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-20 23:40:59 +0300

    Added data types variables

---
+++ Programming Basics Book/ex13page137DataTypesVariables/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ex13page137DataTypesVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //First way - the best way
+            int firstNumber = 5;
+            int secondNumber = 10;
+
+            int temp = firstNumber; // 5
+            firstNumber = secondNumber; // 10
+            secondNumber = temp; // 5
+
+            Console.WriteLine(firstNumber);
+            Console.WriteLine(secondNumber);
+            
+            //Second way - only for integer numbers
+            int a = 2;
+            int b = 3;
+
+            a = a + b; //5
+            b = a - b; // 2
+            a = a - b; // 3
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
---
+++ Programming Basics Book/ex13page137DataTypesVariables/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ex13page137DataTypesVariables")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ex13page137DataTypesVariables")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cf583df2-b5d9-4676-8e80-243333593236")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
