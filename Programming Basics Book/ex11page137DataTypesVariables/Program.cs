bc1aa1ed343c93f1158bc776972d789a4bb658a9
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-20 23:40:28 +0300

    Added data types variables

---
+++ Programming Basics Book/ex11page137DataTypesVariables/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace ex11page137DataTypesVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            //string sign = "\u00A9";
+
+            Console.WriteLine("        ©       ");
+            Console.WriteLine("      ©   ©      ");
+            Console.WriteLine("    ©       ©     ");
+            Console.WriteLine("  ©           ©   ");
+            Console.WriteLine("© © © © © © © © © ");
+            Console.WriteLine();
+        }
+    }
+}
