8b3610d620e9205442308c4d38902176975249a8
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-20 23:40:43 +0300

    Added data types variables

---
+++ Programming Basics Book/ex12page137DataTypesVariables/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ex12page137DataTypesVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter firstname: ");
+                string firstName = Console.ReadLine();
+
+                Console.Write("Enter surname: ");
+                string surName = Console.ReadLine();
+
+                Console.Write("Enter your age: ");
+                int age = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter your gender: ");
+                char gender = char.Parse(Console.ReadLine());
+
+                Console.Write("Enter your ID: ");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Your name is {firstName} {surName}, {age}-years old {gender} with {id} ID.");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
