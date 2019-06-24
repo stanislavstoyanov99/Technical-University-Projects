6c7e30f559e869a60306c87fcbd290b5153a3bf8
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-24 22:14:51 +0300

    Added exercise

---
+++ Second semester/OOPProjects/CarProgram/CarProgram/Automobile.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace CarProgram
+{
+    class Automobile
+    {
+        private int maxSpeed, engineCapacity, currentSpeed;
+        private bool isTheEngineWorks;
+        private string brand, model;
+
+        public Automobile(int maxSpeed, int EngineCapacity, string Brand, string Model)
+        {
+            maxSpeed = MaxSpeed;
+            engineCapacity = EngineCapacity;
+            brand = Brand;
+            model = Model;
+        }
+
+        public bool StartEngine()
+        {
+            currentSpeed = 0;
+            isTheEngineWorks = true;
+
+            return isTheEngineWorks;
+        }
+
+        public bool Accelerate()
+        {
+            bool isTheEngineAccelerate = false;
+
+            if (isTheEngineWorks && currentSpeed <= maxSpeed)
+            {
+                currentSpeed += 5;
+                isTheEngineAccelerate = true;
+            }
+
+            return isTheEngineAccelerate;
+        }
+
+        public bool Brake()
+        {
+            bool isTheEngineBrake = false;
+            if (isTheEngineWorks && currentSpeed <= maxSpeed && currentSpeed >= 40)
+            {
+                currentSpeed -= 40;
+                isTheEngineBrake = true;
+            }
+
+            return isTheEngineBrake;
+        }
+
+        public bool StopEngine()
+        {
+            if (currentSpeed == 0 && isTheEngineWorks)
+            {
+                isTheEngineWorks = false;
+            }
+
+            return isTheEngineWorks;
+        }
+
+        public int CapacityInKw
+        {
+            get { return (int)(engineCapacity * 0.7); }
+        }
+        public int MaxSpeed
+        {
+            get { return maxSpeed; }
+        }
+
+        public int EngineCapacity
+        {
+            get { return engineCapacity; }
+        }
+
+        public int CurrentSpeed
+        {
+            get { return currentSpeed; }
+        }
+
+        public bool IsTheEngineWorks
+        {
+            get { return isTheEngineWorks; }
+        }
+
+        public string Brand
+        {
+            get { return brand; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+        }
+    }
+}
---
+++ Second semester/OOPProjects/CarProgram/CarProgram/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CarProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxSpeedAutomobile1 = int.Parse(Console.ReadLine());
+            int engineCapacityAutomobile1 = int.Parse(Console.ReadLine());
+            string brandAutomobile1 = Console.ReadLine();
+            string modelAutomobile1 = Console.ReadLine();
+
+            int maxSpeedAutomobile2 = int.Parse(Console.ReadLine());
+            int engineCapacityAutomobile2 = int.Parse(Console.ReadLine());
+            string brandAutomobile2 = Console.ReadLine();
+            string modelAutomobile2 = Console.ReadLine();
+
+            Automobile automobile1 = new Automobile(maxSpeedAutomobile1, engineCapacityAutomobile1, brandAutomobile1, modelAutomobile1);
+            Automobile automobile2 = new Automobile(maxSpeedAutomobile2, engineCapacityAutomobile2, brandAutomobile2, modelAutomobile2);
+
+            Console.WriteLine(automobile1.Brand);
+            Console.WriteLine(automobile1.Model);
+            Console.WriteLine(automobile1.MaxSpeed);
+            Console.WriteLine(automobile1.CapacityInKw);
+            Console.WriteLine(automobile1.StartEngine());
+            Console.WriteLine(automobile1.CurrentSpeed);
+            Console.WriteLine(automobile1.Accelerate());
+            Console.WriteLine(automobile1.CurrentSpeed);
+            Console.WriteLine(automobile1.Brake());
+            Console.WriteLine(automobile1.CurrentSpeed);
+            Console.WriteLine(automobile1.StopEngine());
+            Console.WriteLine(automobile1.CurrentSpeed);
+
+            List<string> shapes = new List<string>();
+
+            shapes.Where(x => x.Describe());
+        }
+    }
+}
