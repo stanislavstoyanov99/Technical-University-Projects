3fc490a69c9522c3e424e2fcc0c39a8e8b00a691
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-24 22:15:32 +0300

    Added exercise

---
+++ Second semester/OOPProjects/Delegates/Delegates/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace DelegatesAndEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new EventExample();
+            obj.OnPrinting += PrintingHandler;
+            obj.Print("Hello, world!");
+        }
+
+        static void PrintingHandler(string s)
+        {
+            Console.WriteLine("OnPrinting called");
+        }
+    }
+
+    class Example
+    {
+        public delegate void MyDelegate(string s);
+        public void Print(string s) { Console.Write(s); }
+        public void PrintLine(string s) { Console.WriteLine(s); }
+
+        public void TestDelegate()
+        {
+            MyDelegate d = new MyDelegate(Print);
+            d("Hello, ");
+            d("world!");
+        }
+    }
+
+    public class EventExample
+    {
+        public delegate void MyEventDelegate(string eventDescr);
+        public event MyEventDelegate OnPrinting;
+
+        public void Print(string s)
+        {
+            if (OnPrinting != null)
+            {
+                OnPrinting(s);
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
