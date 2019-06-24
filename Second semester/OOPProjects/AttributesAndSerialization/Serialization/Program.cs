b38e1ab97c9f57a6c8bfee4f37dd8a676d563548
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-24 22:14:16 +0300

    Added exercise

---
+++ Second semester/OOPProjects/AttributesAndSerialization/Serialization/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            Triangle triangle = new Triangle(4, 5, 10);
+
+            triangle._triangles.Add(triangle);
+
+            using (Stream stream = new FileStream("data.db", FileMode.Create, FileAccess.Write))
+            {
+                formatter.Serialize(stream, triangle._triangles);
+            }
+
+            using (Stream stream = new FileStream("data.db", FileMode.Open, FileAccess.Read))
+            {
+                triangle._triangles = (List<Triangle>)formatter.Deserialize(stream);
+            }
+        }
+    }
+}
---
+++ Second semester/OOPProjects/AttributesAndSerialization/Serialization/Triangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Serialization
+{
+    [Serializable]
+    public class Triangle
+    {
+        public List<Triangle> _triangles = new List<Triangle>();
+
+        public int SizeA { get; set; }
+
+        public int SizeB { get; set; }
+
+        public int Height { get; set; }
+
+        public Triangle(int sizeA, int sizeB, int height)
+        {
+            sizeA = SizeA;
+            sizeB = SizeB;
+            height = Height;
+        }
+    }
+}
