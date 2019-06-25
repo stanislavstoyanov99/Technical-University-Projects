6a77592a8b8e4f8d69b3117c595645ba09ef505d
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-25 23:42:48 +0300

    Added exercise

---
+++ Second semester/OOPProjects/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point triangleCentre = new Point(3, 4);
+            Point rectangleCentre = new Point(7, 5);
+
+            Triangle triangle = new Triangle(triangleCentre, 6, 6, 6);
+            Rectangle rectangle = new Rectangle(rectangleCentre, 5, 5);
+            Shape shape = new Triangle(triangleCentre, 4, 4, 4);
+
+            List<Shape> shapes = new List<Shape>
+            {
+                triangle,
+                rectangle,
+                shape
+            };
+
+            foreach (Shape sh in shapes)
+            {
+                sh.Describe();
+            }
+        }
+    }
+
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    public class Shape
+    {
+        protected Point Centre { get; }
+
+        public Shape(Point centre)
+        {
+            Centre = centre;
+        }
+
+        public virtual void Describe()
+        {
+            Console.WriteLine($"This is a shape with centre coordinates {Centre.X}, {Centre.Y} ");
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        public int A { get; }
+        public int B { get; }
+        public int C { get; }
+
+        public Triangle(Point centre, int a, int b, int c) : base(centre)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public override void Describe()
+        {
+            Console.WriteLine($"This is a triange with centre coordinates {Centre.X}, {Centre.Y} with sides {A}, {B} and {C}");
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public int A { get; }
+        public int B { get; }
+
+        public Rectangle(Point centre, int a, int b) : base(centre)
+        {
+            A = a;
+            B = b;
+        }
+
+        public override void Describe()
+        {
+            Console.WriteLine($"This is a rectangle with centre coordinates {Centre.X}, {Centre.Y} with sides {A} and {B}");
+        }
+    }
+}
