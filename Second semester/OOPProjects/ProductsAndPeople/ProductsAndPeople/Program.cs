90d875400d43c24cb10d2264c7ea22750785bede
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-06-26 12:53:47 +0300

    Added exercise

---
+++ Second semester/OOPProjects/ProductsAndPeople/ProductsAndPeople/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ProductsAndPeople
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var r = people.GroupJoin(items, o => o.Id, i => i.PersonId, (o,i) => 
+            // new { Person = o, Products = i.Join(products, o2 => o2.ProductId, i2 => i2.Id, (o2,i2) => i2).ToArray()}).ToArray();
+
+            // var r = people.GroupJoin(items, o => o.Id, i => i.PersonId, (o,i) => 
+            // new { Person = o, Products = i.Join(products, o2 => o2.ProductId, i2 => i2.Id, (o2,i2) => i2.Type + i2.Model).Aggregate((f,s) => f+s)}).ToArray();
+
+            // Without GroupJoin and Join
+
+            // var r = people.Select(s => new { Person = s, Products = items. Where(c => c.PersonId == s.Id).Select( s2 => products.Where(c2 => c2.Id == s2.ProductId).SingleOrDefault()).ToArray()}.ToArray();
+        }
+    }
+}
