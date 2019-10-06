16568c9e3e4eb82c75dd5d71155d027bcf6f7780
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-10-06 23:00:35 +0300

    Added java first exercises

---
+++ Third semester/Java/Exercises/CurrencyConverter/src/currencyConverter.java
@@ -0,0 +1,41 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class currencyConverter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("#.00");
+
+        System.out.println("Which currency do you want to convert?");
+        System.out.println("1: Pound to BGN \t2: Euro to BGN \t3: Dollar to BGN");
+        int code = sc.nextInt();
+
+        System.out.println("How much money do you want to convert?");
+        double inputSum = sc.nextDouble();
+
+        double resultSum = ConvertInputSum(code, inputSum, df);
+    }
+
+    public static double ConvertInputSum(int code, double inputSum, DecimalFormat df) {
+        double resultSum = 0;
+
+        switch (code) {
+            case 1:
+                resultSum = inputSum * 2.53405;
+                System.out.println("Your " + inputSum + " pound is: " + df.format(resultSum) + " BGN");
+                break;
+            case 2:
+                resultSum = inputSum / 1.95583;
+                System.out.println("Your " + inputSum + " euro is: " + df.format(resultSum) + " BGN");
+                break;
+            case 3:
+                resultSum = inputSum * 1.79549;
+                System.out.println("Your " + inputSum + " dollars is: " + df.format(resultSum) + " BGN");
+                break;
+            default:
+                System.out.println("Invalid choice for currency.");
+        }
+
+        return resultSum;
+    }
+}
