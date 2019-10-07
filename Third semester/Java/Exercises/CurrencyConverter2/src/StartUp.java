50fe0a479e33f65eaeb56952e7720152cc47167f
Stanislav Stoyanov <45464117+stanislavstoyanov99@users.noreply.github.com>
2019-10-07 23:16:15 +0300

    Delete StartUp.java

--- Third semester/Java/Exercises/CurrencyConverter2/src/StartUp.java
+++
@@ -1,8 +0,0 @@
-public class StartUp {
-    public static void main(String[] args) {
-        CurrencyConverter cc = new CurrencyConverter();
-        double[] rates = {63.0, 3.0, 3.0, 595.0, 18.0, 107.0, 2.0};
-        cc.setExchangeRates(rates);
-        cc.printCurrencies();
-    }
-}
