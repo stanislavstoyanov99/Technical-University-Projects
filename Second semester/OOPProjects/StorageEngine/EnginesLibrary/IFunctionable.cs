224ee6350291c617ab24b93b74a61602a729e93d
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-11-30 16:59:07 +0200

    Deleteted project

--- Second semester/OOPProjects/StorageEngine/EnginesLibrary/IFunctionable.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EnginesLibrary
-{
-    public interface IFunctionable
-    {
-        void RotateClockwize(int voltage, int rpm);
-        void RotateCounterClockwize(int voltage, int rpm);
-        void CalculateKPD(double capacity, int voltage, int amperage);
-    }
-}
