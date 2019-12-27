52bba02ff3c3f27977dc521d31402cb6df7bf10d
Stanislav Stoyanov <slavkata_99@abv.bg>
2019-12-27 22:41:29 +0200

    Successfully done hibernate example!

--- Third semester/Java/Exercises/Lab/Exercise8AndExercise9/HibernateJPABeginner/src/main/java/UserDAOTest.java
+++ Third semester/Java/Exercises/Lab/Exercise8AndExercise9/HibernateJPABeginner/src/main/java/UserDAOTest.java
@@ -1,4 +1,5 @@
 import net.codejava.hibernate.models.User;
+import org.hibernate.Session;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -12,6 +13,7 @@ public class UserDAOTest {
 
         entityManager.getTransaction().begin();
 
+        /* Creates user and adds it to the database
         User newUser = new User();
         newUser.setEmail("billjoy@gmail.com");
         newUser.setFullname("Bill Joy");
@@ -19,9 +21,9 @@ public class UserDAOTest {
 
         entityManager.persist(newUser);
         entityManager.getTransaction().commit();
-        entityManager.close();
-        factory.close();
+         */
 
+        /* Updates existing user
         User existingUser = new User();
 
         existingUser.setId(1);
@@ -30,14 +32,18 @@ public class UserDAOTest {
         existingUser.setPassword("billionaire");
 
         entityManager.merge(existingUser);
+         */
 
+        /* Find an entity instance
         Integer primaryKey = 1;
         User user = entityManager.find(User.class, primaryKey);
 
         System.out.println(user.getEmail());
         System.out.println(user.getFullname());
         System.out.println(user.getPassword());
+        */
 
+        /* Execute a query
         String sql = "SELECT u from User u where u.email = 'bill.joy@gmail.com'";
         Query query = entityManager.createQuery(sql);
         User foundUser = (User) query.getSingleResult();
@@ -45,9 +51,16 @@ public class UserDAOTest {
         System.out.println(foundUser.getEmail());
         System.out.println(foundUser.getFullname());
         System.out.println(foundUser.getPassword());
+        */
 
-        /*Integer primaryKey = 1;
+        /* Remove an entity instance
+        Integer primaryKey = 1;
         User reference = entityManager.getReference(User.class, primaryKey);
-        entityManager.remove(reference);*/
+        entityManager.remove(reference);
+         */
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        factory.close();
     }
 }
