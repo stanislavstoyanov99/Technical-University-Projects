be08da96310f2e9294d334428a70b8f0eacb7860
Stanislav Stoyanov <slavkata_99@abv.bg>
2020-03-24 20:29:15 +0200

    Add new exercises for subjects

---
+++ Fourth semester/Operations Research/Exercises/IO/IO/StartUp.cs
@@ -0,0 +1,49 @@
+﻿namespace IO
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int[] A = { 100, 130, 170 };
+            int[] B = { 150, 120, 80, 50 };
+            int[,] costs = new int[,] { { 3, 5, 7, 11 }, { 1, 4, 6, 3 }, { 5, 8, 12, 7 } };
+            int[,] solution = new int[,] { { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+
+            int i = 0;
+            int j = 0;
+
+            while (true)
+            {
+                if (A[i] > B[j])
+                {
+                    solution[i, j] = B[j];
+                    A[i] = A[i] - B[j];
+                    j++;
+                }
+                else if (A[i] < B[j])
+                {
+                    solution[i, j] = A[i];
+                    B[j] = B[j] - A[i];
+                    i++;
+                }
+                else if (A[i] == B[j])
+                {
+                    solution[i, j] = A[i];
+                    break;
+                }
+            }
+
+            for (int row = 0; row < solution.GetLength(0); row++)
+            {
+                for (int col = 0; col < solution.GetLength(1); col++)
+                {
+                    Console.Write(solution[row, col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Fourth semester/Operations Research/Exercises/IO/IO2/StartUp.cs
@@ -0,0 +1,70 @@
+﻿namespace IO2
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int[] A = { 100, 130, 170 }; // Масив съдържащ наличностите на складовете / производителите
+            int[] B = { 150, 120, 80, 50 }; // Масив съдържащ потребностите
+            int[,] costs = new int[,] { { 3, 5, 7, 11 }, { 1, 4, 6, 3 }, { 5, 8, 12, 7 } }; // Матрица на разходите
+            int[,] solution = new int[,] { { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } }; // Матрица на решението
+
+            int io = 0;
+            int jo = 0;
+            int m = 3; // брой складове
+            int n = 4; // брой потребители
+
+            double minElement = costs[0, 0];
+            double M = Math.Pow(10, 5);
+
+            while (true)
+            {
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (costs[i, j] < minElement && A[i] != 0 && B[j] != 0)
+                        {
+                            minElement = costs[i, j];
+                            io = i;
+                            jo = j;
+                        }
+                    }
+                }
+
+                if (A[io] > B[jo])
+                {
+                    solution[io, jo] = B[jo];
+                    A[io] = A[io] - B[jo];
+                    B[jo] = 0;
+                }
+                else if (A[io] < B[jo])
+                {
+                    solution[io, jo] = A[io];
+                    B[jo] = B[jo] - A[io];
+                    A[io] = 0;
+                }
+                else if (A[io] == B[jo])
+                {
+                    solution[io, jo] = A[io];
+                    break;
+                }
+
+                minElement = M;
+            }
+
+            // Принтиране на матрицата
+            for (int row = 0; row < solution.GetLength(0); row++)
+            {
+                for (int col = 0; col < solution.GetLength(1); col++)
+                {
+                    Console.Write(solution[row, col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
