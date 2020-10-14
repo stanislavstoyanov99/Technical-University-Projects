1ec78b1f60f82014e22bae8a6d06c61906e44bf7
Stanislav Stoyanov <slavkata_99@abv.bg>
2020-10-14 19:30:38 +0300

    Add cryptography homework.

---
+++ Fifth semester/Cryptography/Exercises/DirectReplacement/DirectReplacement/Startup.cs
@@ -0,0 +1,98 @@
+﻿namespace DirectReplacement
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Startup
+    {
+        // This program is an algorithm for direct replacement - example 2.2 - page 17 from the guide
+        public static void Main()
+        {
+            var bulgarianAlphabet = Enumerable.Range('А', 'Я' - 'А' + 1).Select(i => ((char)i).ToString()).ToArray();
+            bulgarianAlphabet = bulgarianAlphabet.Where(i => i != "Ы" && i != "Э").ToArray();
+            var digits = Enumerable.Range(0, 10).Select(i => i.ToString()).ToArray();
+            string[] specialSymbols = { " ", ".", "-", "#" };
+
+            var allowedSymbols = string.Join("", bulgarianAlphabet)
+                + ""
+                + string.Join("", digits)
+                + ""
+                + string.Join("", specialSymbols);
+
+            int[] symbolsToReplace = 
+                { 10, 20, 30, 40, 15,
+                  25, 11, 21, 31, 41,
+                  35, 45, 12, 22, 32,
+                  42, 13, 23, 33, 43,
+                  14, 24, 34, 16, 26,
+                  36, 17, 27, 37, 18,
+                  28, 38, 19, 29, 39,
+                  1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            Console.Write("Write input text for encryption: ");
+            var inputText = Console.ReadLine();
+
+            if (inputText.Length > 300)
+            {
+                Console.WriteLine("The input text should not be more than 300 symbols.");
+                return;
+            }
+
+            var encryptedText = new List<string>();
+            var decryptedText = new StringBuilder();
+
+            try
+            {
+                for (int i = 0; i < inputText.Length; i++)
+                {
+                    var doesItExist = allowedSymbols.Any(x => x == inputText[i]);
+
+                    if (doesItExist)
+                    {
+                        var currentLetter = allowedSymbols.First(x => x == inputText[i]);
+
+                        if (!char.IsWhiteSpace(currentLetter))
+                        {
+                            var newPosition = allowedSymbols.IndexOf(currentLetter);
+                            encryptedText.Add(symbolsToReplace[newPosition] + " ");
+                        }
+                        else
+                        {
+                            encryptedText.Add(" ");
+                        }
+                    }
+                    else
+                    {
+                        throw new ArgumentException("The letter in the input text does not exist in the allowed symbols.");
+                    }
+                }
+
+                Console.WriteLine($"Encrypted text: {string.Join("", encryptedText)}");
+
+                for (int i = 0; i < encryptedText.Count; i++)
+                {
+                    var currentValue = encryptedText[i];
+
+                    if (currentValue != " ")
+                    {
+                        var oldPosition = Array.IndexOf(symbolsToReplace, int.Parse(encryptedText[i]));
+                        var initialLetter = allowedSymbols[oldPosition];
+                        decryptedText.Append(initialLetter);
+                    }
+                    else
+                    {
+                        decryptedText.Append(" ");
+                    }
+                }
+
+                Console.WriteLine($"Decrypted text: {decryptedText}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Fifth semester/Cryptography/Exercises/DirectReplacementHW/DirectReplacementHW/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace DirectReplacementHW
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    // This program is an algorithm for direct replacement - homework page 17
+    public class Program
+    {
+        public static void Main()
+        {
+            var allowedSymbols = new Dictionary<string, int>()
+            {
+                {"А", 1 }, {"Б", 2 }, {"В", 3 }, {"Г", 4 },
+                {"Д", 5 }, {"Е", 6 }, {"И", 7 }, {"К", 8 },
+                {"Л", 9 }, {"М", 10 }, {"Н", 11 }, {"О", 12 },
+                {"П", 13 }, {"Р", 14 }, {"С", 15 }, {"Т", 16 },
+                {"У", 17 }, {"Ф", 18 }, {"Х", 19 }, {"Я", 20 },
+                {"0", 21 }, {"1", 22 }, {"2", 23 }, {"3", 24 },
+                {"4", 25 }, {"5", 26 }, {"6", 27 }, {"7", 28 },
+                {"8", 29 }, {"9", 30 }, {"N", 31 }, {"Y", 32 },
+                {"U", 33 }, {"S", 40 }, {"D", 34 }, {" ", 35 },
+                {"\"", 39 }, {"'", 36 }, {"-", 37 }, {"Ъ", 38 },
+            };
+
+            Console.Write("Write input text for encryption: ");
+            var inputText = Console.ReadLine();
+
+            if (inputText.Length > 300)
+            {
+                Console.WriteLine("The input text should not be more than 300 symbols.");
+                return;
+            }
+
+            var encryptedText = new List<string>();
+            var decryptedText = new StringBuilder();
+
+            // Logic with interval in allowedSymbols
+            try
+            {
+                for (int i = 0; i < inputText.Length; i++)
+                {
+                    if (allowedSymbols.ContainsKey(inputText[i].ToString()))
+                    {
+                        var encryptedCode = allowedSymbols.First(x => x.Key == inputText[i].ToString()).Value;
+                        encryptedText.Add(encryptedCode + " ");
+                    }
+                    else
+                    {
+                        throw new ArgumentException("The letter in the input text does not exist in the allowed symbols.");
+                    }
+                }
+
+                Console.WriteLine($"Encrypted text: {string.Join(" ", encryptedText)}");
+
+                for (int i = 0; i < encryptedText.Count; i++)
+                {
+                    var value = encryptedText[i];
+                    var decryptedLetter = allowedSymbols.First(x => x.Value == int.Parse(encryptedText[i])).Key;
+                    decryptedText.Append(decryptedLetter);
+                }
+
+                Console.WriteLine($"Decrypted text: {decryptedText.ToString().TrimEnd()}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
