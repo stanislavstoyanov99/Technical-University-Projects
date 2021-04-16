e33d57678c11f6db6920b85e37643d4f63b8cb59
Stanislav Stoyanov <slavkata_99@abv.bg>
2021-04-16 11:45:36 +0300

    Add Textures exercise from Computer graphic course

---
+++ Sixth semester/Computer graphic/Exercises/OpenGL1Textures/OpenGL1/Shader.h
@@ -0,0 +1,29 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <glad/glad.h> // include glad to get all the required OpenGL headers
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+class Shader
+{
+public:
+    // the program ID
+    unsigned int ID;
+
+    // constructor reads and builds the shader
+    Shader(const char* vertexPath, const char* fragmentPath);
+	
+    // use/activate the shader
+    void Use();
+	
+    // utility uniform functions
+    void SetBool(const std::string& name, bool value) const;
+    void SetInt(const std::string& name, int value) const;
+    void SetFloat(const std::string& name, float value) const;
+};
+
+#endif
